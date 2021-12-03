c104bb996609c5aa0923a29b8b73b8ed0123fae0
仲浩杰 <zhonghaojie@loctek.com>
2021-12-03 16:15:45 +0800

    增加JSONConverterFactory

--- base/src/main/java/com/lege/android/base/retrofit/JSONConverterFactory.java
+++ base/src/main/java/com/lege/android/base/retrofit/JSONConverterFactory.java
@@ -1,8 +1,8 @@
 package com.lege.android.base.retrofit;
 
 import com.blankj.utilcode.util.GsonUtils;
-import com.google.gson.Gson;
-import com.lege.android.base.util.JSON;
+
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.lang.annotation.Annotation;
@@ -20,7 +20,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
  * Description:
  * Created by loctek on 2021/12/3.
  */
-public class JSONConverterFactory<T> extends Converter.Factory {
+public class JSONConverterFactory extends Converter.Factory {
     private static final MediaType MEDIA_TYPE = MediaType.parse("application/json;charset=UTF-8");
     private static final Charset UTF_8 = Charset.forName("utf-8");
 
@@ -29,20 +29,20 @@ public class JSONConverterFactory<T> extends Converter.Factory {
     }
 
     @Override
-    public Converter<T, RequestBody> requestBodyConverter(Type type, Annotation[] parameterAnnotations, Annotation[] methodAnnotations, Retrofit retrofit) {
-        return new Converter<T, RequestBody>() {
+    public Converter<JSONObject, RequestBody> requestBodyConverter(Type type, Annotation[] parameterAnnotations, Annotation[] methodAnnotations, Retrofit retrofit) {
+        return new Converter<JSONObject, RequestBody>() {
             @Override
-            public RequestBody convert(T value) throws IOException {
+            public RequestBody convert(JSONObject value) throws IOException {
                 return RequestBody.create(MEDIA_TYPE, GsonUtils.toJson(value).getBytes(UTF_8));
             }
         };
     }
 
     @Override
-    public Converter<ResponseBody, T> responseBodyConverter(final Type type, Annotation[] annotations, Retrofit retrofit) {
-        return new Converter<ResponseBody, T>() {
+    public Converter<ResponseBody, JSONObject> responseBodyConverter(final Type type, Annotation[] annotations, Retrofit retrofit) {
+        return new Converter<ResponseBody, JSONObject>() {
             @Override
-            public T convert(ResponseBody value) throws IOException {
+            public JSONObject convert(ResponseBody value) throws IOException {
                 return GsonUtils.fromJson(value.string(), type);
             }
         };
