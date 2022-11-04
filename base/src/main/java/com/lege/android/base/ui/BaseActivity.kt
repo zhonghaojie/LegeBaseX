e87783cfea24fdf19a94286c1d914f510e911f0a
zhonghaojie <zhj1994317>
2022-11-04 21:42:38 +0800

    显示状态栏

--- base/src/main/java/com/lege/android/base/ui/BaseActivity.kt
+++ base/src/main/java/com/lege/android/base/ui/BaseActivity.kt
@@ -46,7 +46,7 @@ open class BaseActivity : SupportActivity(), SwipeBackActivityBase {
 //                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 //                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
 //                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                                 | View.SYSTEM_UI_FLAG_FULLSCREEN       // hide status bar
+//                                 | View.SYSTEM_UI_FLAG_FULLSCREEN       // hide status bar
 //                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
 //        systemUiVisibility = systemUiVisibility or flags
 //        decorView.systemUiVisibility = systemUiVisibility
