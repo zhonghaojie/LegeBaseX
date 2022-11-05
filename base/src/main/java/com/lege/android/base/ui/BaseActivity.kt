76f3e876adb5359e589a4f12454d9486cbd90949
zhonghaojie <zhj1994317>
2022-11-05 10:23:36 +0800

    隐藏状态栏

--- base/src/main/java/com/lege/android/base/ui/BaseActivity.kt
+++ base/src/main/java/com/lege/android/base/ui/BaseActivity.kt
@@ -37,19 +37,19 @@ open class BaseActivity : SupportActivity(), SwipeBackActivityBase {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         BaseActivityCollector.getInstance().addActivity(this)
-//        val mActionBar = supportActionBar
-//        mActionBar?.hide()
-//        val decorView = window.decorView
-//        var systemUiVisibility = decorView.systemUiVisibility
-//        val flags = (View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-//                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-//                //                 | View.SYSTEM_UI_FLAG_FULLSCREEN       // hide status bar
-//                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
-//        systemUiVisibility = systemUiVisibility or flags
-//        decorView.systemUiVisibility = systemUiVisibility
-//        window.statusBarColor = Color.TRANSPARENT
+        val mActionBar = supportActionBar
+        mActionBar?.hide()
+        val decorView = window.decorView
+        var systemUiVisibility = decorView.systemUiVisibility
+        val flags = (View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                //                 | View.SYSTEM_UI_FLAG_FULLSCREEN       // hide status bar
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+        systemUiVisibility = systemUiVisibility or flags
+        decorView.systemUiVisibility = systemUiVisibility
+        window.statusBarColor = Color.TRANSPARENT
         APPLog.log(BASE_ACTIVITY_TAG, javaClass.simpleName + "   " + "onCreate")
         mHelper = SwipeBackActivityHelper(this)
         mHelper?.onActivityCreate()
