11cba209d7995cce838c1273a766e54b194ec2b1
zhonghaojie <zhj1994317>
2022-11-05 09:54:25 +0800

    隐藏状态栏

--- base/src/main/java/com/lege/android/base/ui/BaseActivity.kt
+++ base/src/main/java/com/lege/android/base/ui/BaseActivity.kt
@@ -39,18 +39,17 @@ open class BaseActivity : SupportActivity(), SwipeBackActivityBase {
         BaseActivityCollector.getInstance().addActivity(this)
         val mActionBar = supportActionBar
         mActionBar?.hide()
-
-//        val decorView = window.decorView
-//        var systemUiVisibility = decorView.systemUiVisibility
-//        val flags = (View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-//                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-//                                 | View.SYSTEM_UI_FLAG_FULLSCREEN       // hide status bar
-//                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
-//        systemUiVisibility = systemUiVisibility or flags
-//        decorView.systemUiVisibility = systemUiVisibility
-//        window.statusBarColor = Color.TRANSPARENT
+        val decorView = window.decorView
+        var systemUiVisibility = decorView.systemUiVisibility
+        val flags = (View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                //                 | View.SYSTEM_UI_FLAG_FULLSCREEN       // hide status bar
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+        systemUiVisibility = systemUiVisibility or flags
+        decorView.systemUiVisibility = systemUiVisibility
+        window.statusBarColor = Color.TRANSPARENT
         APPLog.log(BASE_ACTIVITY_TAG, javaClass.simpleName + "   " + "onCreate")
         mHelper = SwipeBackActivityHelper(this)
         mHelper?.onActivityCreate()
@@ -186,6 +185,28 @@ open class BaseActivity : SupportActivity(), SwipeBackActivityBase {
         }
     }
 
+    override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
+        Log.e(BASE_ACTIVITY_TAG,"${javaClass.simpleName}  onTouchEvent  ${ev?.action}" )
+        if(ev?.action == MotionEvent.ACTION_DOWN){
+            Log.e(BASE_ACTIVITY_TAG, javaClass.simpleName + "   " + "ACTION_DOWN   ${ev.y}")
+            if (ev.y < 5f) {
+                val intent = Intent("show.lege.pullbar")
+                intent.putExtra("show.lege.pullbar.x",ev.x)
+                LocalBroadcastManager.getInstance(this).sendBroadcast(intent)
+                Log.e(BASE_ACTIVITY_TAG,"sendBroadcast " )
+            }
+        }
+        if (ThemeAndScreenManager.instance.screenProtect) {
+            if (ev.action == MotionEvent.ACTION_UP) {
+                if (needStartProtectCounter()) {
+                    TaskSingleInstance.getInstance().startTimer()
+                }
+            }
+        } else {
+            TaskSingleInstance.getInstance().stopTimer()
+        }
+        return super.dispatchTouchEvent(ev)
+    }
 
     protected open fun needToAvoidQuicklyClick(): Boolean {
         return true
