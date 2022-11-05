21357aa10b81e834f39db4b6a2edc48b2a85d321
zhonghaojie <zhj1994317>
2022-11-05 13:15:09 +0800

    修改systemUiVisibility

--- base/src/main/java/com/lege/android/base/ui/BaseActivity.kt
+++ base/src/main/java/com/lege/android/base/ui/BaseActivity.kt
@@ -41,14 +41,7 @@ open class BaseActivity : SupportActivity(), SwipeBackActivityBase {
         mActionBar?.hide()
         val decorView = window.decorView
         var systemUiVisibility = decorView.systemUiVisibility
-        val flags = (
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-//                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                        or View.SYSTEM_UI_FLAG_FULLSCREEN       // hide status bar
-                        or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
-        systemUiVisibility = systemUiVisibility or flags
+        systemUiVisibility = systemUiVisibility or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
         decorView.systemUiVisibility = systemUiVisibility
         window.statusBarColor = Color.TRANSPARENT
         APPLog.log(BASE_ACTIVITY_TAG, javaClass.simpleName + "   " + "onCreate")
